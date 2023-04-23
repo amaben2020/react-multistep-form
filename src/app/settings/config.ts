3fe63df8171f12373e527c6a409b3151da8131d5
algomachine007 <amaben202phantom@gmail.com>
2023-04-23 20:46:39 +0100

    adding interceptors

--- src/app/settings/config.ts
+++ src/app/settings/config.ts
@@ -10,3 +10,30 @@ axiosClient.defaults.headers = {
 } as any;
 
 axiosClient.defaults.withCredentials = true;
+
+// https://masteringjs.io/tutorials/axios/interceptors
+
+//Axios interceptors are functions that Axios calls for every request
+axiosClient.interceptors.request.use((req) => {
+  console.log(`${req.method} ${req.url}`);
+  // Important: request interceptors **must** return the request.
+  return req;
+});
+
+axiosClient.interceptors.response.use((res) => {
+  console.log("Returned data", res.data);
+  // Important: response interceptors **must** return the response.
+  return res;
+});
+
+// Setting headers
+axiosClient.interceptors.request.use((req) => {
+  // `req` is the Axios request config, so you can modify
+  // the `headers`.
+  req.headers.authorization = "my secret token which may be a test";
+
+  req.headers["Content-Type"] = "application/json";
+  return req;
+});
+
+// Error handling
